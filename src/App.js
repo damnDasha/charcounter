e749f3ed80d623fe27db9b16905e1e83099f14d1
dasha akhten <dasha.akhten@gmail.com>
2021-10-21 20:21:45 -0700

    feat: character counter

--- src/App.js
+++ src/App.js
@@ -1,41 +1,21 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import CharacterCounter from "react-character-counter";
 
-class App extends Component {
-  state = {
-    name: "",
-  };
+function App() {
+  const [charCount, setCharCount] = useState(0);
 
-  render() {
-    // value: String;
-    // wrapperStyle: Object;
-    // characterCounterStyle: Object;
-    // overrideStyle: true;
+  return (
+    <div className="app">
+      <form className="charCount">
+        <h1>character counter</h1>
+        <textarea
+          onChange={(e) => setCharCount(e.target.value.length)}
+          placeholder="enter text here"
+        ></textarea>
 
-    return (
-      <div className="app">
-        <form className="charCount">
-          <h1>character counter</h1>
-          <textarea></textarea>
-          <CharacterCounter
-            value={this.state.name}
-            wrapperStyle={{ position: "relative" }}
-            characterCounterStyle={{
-              position: "absolute",
-              fontSize: "12px",
-              fontWeight: "600",
-              top: "14px",
-              right: "0px",
-            }}
-          >
-            <input type="text" value={this.state.name} />
-          </CharacterCounter>
-          <p>characters</p>
-        </form>
-      </div>
-    );
-  }
+        <p>characters: {charCount}</p>
+      </form>
+    </div>
+  );
 }
-
 export default App;
