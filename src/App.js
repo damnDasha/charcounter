f2249b672c5d48aa931abb45c1a17b72c37a28c5
dasha akhten <dasha.akhten@gmail.com>
2021-10-21 19:48:01 -0700

    feat: start react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import CharacterCounter from "react-character-counter";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    name: "",
+  };
+
+  render() {
+    // value: String;
+    // wrapperStyle: Object;
+    // characterCounterStyle: Object;
+    // overrideStyle: true;
+
+    return (
+      <div className="app">
+        <form className="charCount">
+          <h1>character counter</h1>
+          <textarea></textarea>
+          <CharacterCounter
+            value={this.state.name}
+            wrapperStyle={{ position: "relative" }}
+            characterCounterStyle={{
+              position: "absolute",
+              fontSize: "12px",
+              fontWeight: "600",
+              top: "14px",
+              right: "0px",
+            }}
+          >
+            <input type="text" value={this.state.name} />
+          </CharacterCounter>
+          <p>characters</p>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
