c79b4ac6d7ba5d754b0862147f807c8ff634982e
dasha akhten <dasha.akhten@gmail.com>
2021-10-22 20:57:31 -0700

    fix: not a function kek

--- src/App.js
+++ src/App.js
@@ -1,21 +1,51 @@
-import React, { useState } from "react";
+// function App() {
+// const counts = {};
+
+//   return (
+//     <div className="app">
+//       <form className="charCount">
+//         <h1>character counter</h1>
+//         <textarea
+//         onChange={(e => text.split('').filter(e => e != ' ').map(e => counts[e] ? counts[e]++ : counts[e] = 1)
+
+//         ></textarea>
+
+//         <p>characters: {counts}</p>
+//       </form>
+//     </div>
+//   );
+// }
+// export default App;
+
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-  const [charCount, setCharCount] = useState(0);
+export class App extends Component {
+  props = {
+    counts: [],
+    text: [],
+  };
+  handleOnChange = (e) => {
+    e.preventDefault();
+    const counts = [];
+    const text = [];
 
-  return (
-    <div className="app">
-      <form className="charCount">
-        <h1>character counter</h1>
-        <textarea
-          onChange={(e) => setCharCount(e.target.value.length)}
-          placeholder="enter text here"
-        ></textarea>
-
-        <p>characters: {charCount}</p>
-      </form>
-    </div>
-  );
+    text
+      .split("")
+      .filter((x) => x !== " ")
+      .map((x) => (counts[x] ? counts[x]++ : (counts[x] = 1)));
+  };
+  render() {
+    return (
+      <div className="charCount">
+        <form className="charCount">
+          <h1>character counter</h1>
+          <textarea onChange={this.handleOnChange}>{this.props.text}</textarea>
+          <p>characters: {this.props.counts}</p>
+        </form>
+      </div>
+    );
+  }
 }
+
 export default App;
